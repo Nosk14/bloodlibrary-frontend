9db97d3d56d9c298aa1c76afcdb32b5ea8a5b73e
Carlos Lucas <nosk14@gmail.com>
2021-09-28 15:26:02 +0200

    button to open all favorite cards

--- bloodlibrary/static/pod/pod_scripts.js
+++ bloodlibrary/static/pod/pod_scripts.js
@@ -17,6 +17,8 @@ var icons = {
     'Follower of Set': "https://static.krcg.org/clan/followersofset.svg",
 };
 
+var affiliate_param = '?affiliate_id=2900918';
+
 var full_star_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>';
 var empty_star_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/></svg>';
 
@@ -93,10 +95,10 @@ function addCards(cardList) {
             `<div id="${c.id}" class="vtes-card" data-release='${c.release_date}' data-name="${c.name}" data-cardtype='${c.type[0]}' data-favorite='2000-01-01'>
                     <table><tbody>
                         <tr><td>
-                            <a href="${c.link}?affiliate_id=2900918" target="_blank"><img class="pod-image" src="https://statics.bloodlibrary.info/img/sets/pod/${c.id}.jpg"/></a>
+                            <a href="${c.link}${affiliate_param}" target="_blank"><img class="pod-image" src="https://statics.bloodlibrary.info/img/sets/pod/${c.id}.jpg"/></a>
                         </td></tr>
                         <tr><td class="vtes-card-name red-text">
-                            <a class="favorite-icon" onclick="toggleFavorite(this,${c.id})" >${localStorage.getItem(c.id) === null ? empty_star_icon : full_star_icon}</a> <a href="${c.link}?affiliate_id=2900918" target="_blank">${c.name}</a>
+                            <a class="favorite-icon" onclick="toggleFavorite(this,${c.id})" >${localStorage.getItem(c.id) === null ? empty_star_icon : full_star_icon}</a> <a href="${c.link}${affiliate_param}" target="_blank">${c.name}</a>
                         </td></tr>
                     </tbody></table>
             </div>`
@@ -184,4 +186,16 @@ function toggleFavorite(self, card_id){
         self.innerHTML = empty_star_icon;
     }
 
+}
+
+function openAllFavoriteCards(){
+    for (c of cards) {
+        if (localStorage.getItem(c.id) !== null){
+            let result = window.open(c.link+affiliate_param, '_blank');
+            if (result === null){
+                $('#open-tabs').append('<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Oops!</strong>Your browser blocked some of the opening tabs. Check your settings to fix it. <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+                break;
+            }
+        }
+    }
 }
