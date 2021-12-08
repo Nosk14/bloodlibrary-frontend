447a9f8aebe9ed256103030cfb7ce46bbfae40b5
Carlos Lucas <nosk14@gmail.com>
2021-12-08 11:36:45 +0100

    remove duplicated Promo cards on all_cards view

--- bloodlibrary/features/cards.py
+++ bloodlibrary/features/cards.py
@@ -4,3 +4,13 @@ BLOODLIBRARY_SETS_ENDPOINT = "https://api.bloodlibrary.info/api/sets"
 
 rs = requests.get(BLOODLIBRARY_SETS_ENDPOINT)
 all_sets_data = rs.json()
+
+for zet in all_sets_data:
+    if zet['name'] == 'Promo':
+        added_cards = dict()
+        for card in zet['cards']:
+            if card['id'] not in added_cards or added_cards[card['id']]['image'] is None and card['image'] is not None:
+                added_cards[card['id']] = card
+        zet['cards'] = list(added_cards.values())
+
+    zet['cards'].sort(key=lambda c: c['id'])
